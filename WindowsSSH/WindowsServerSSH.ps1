84dd087a7fd519a54bcb8ede456d26c07e90437b
Venkata Sai Hitesh Jonnalagadda <saihitesh98@gmail.com>
2021-12-15 14:56:46 +0900

    Update WindowsServerSSH.ps1

--- WindowsSSH/WindowsServerSSH.ps1
+++ WindowsSSH/WindowsServerSSH.ps1
@@ -50,7 +50,7 @@ New-ItemProperty -Path "HKLM:\SOFTWARE\OpenSSH" -Name DefaultShell -Value "C:\Wi
 
 try{
     # Copy key
-    New-Item -Path "C:\Users\sre-admin-local\.ssh" -Name ".ssh" -ItemType "directory"
+    New-Item -Path "C:\Users\sre-admin-local\" -Name ".ssh" -ItemType "directory"
     (Invoke-WebRequest -UseBasicParsing "https://gist.githubusercontent.com/hiteshsai/871dc14372e07d98efbeadb1390d3eca/raw/54ffd3dbb111ce053770c391207d7f3ee737a673/authorized_keys.key").content | Out-File C:\Users\sre-admin-local\.ssh\authorized_keys -Encoding ascii
     # Set permissions on the file
     Get-Acl "$env:ProgramData\ssh\ssh_host_dsa_key" | Set-Acl C:\Users\sre-admin-local\.ssh\authorized_keys
@@ -58,8 +58,8 @@ try{
     # set ssh key usage
     ((Get-Content -path $env:ProgramData\ssh\sshd_config -Raw) `
     -replace '#PubkeyAuthentication yes','PubkeyAuthentication yes' `
-    -replace '#PasswordAuthentication yes','PasswordAuthentication no'
-    -replace 'Match Group administrators','#Match Group administrators'
+    -replace '#PasswordAuthentication yes','PasswordAuthentication no'`
+    -replace 'Match Group administrators','#Match Group administrators'`
     -replace '       AuthorizedKeysFile __PROGRAMDATA__/ssh/administrators_authorized_keys','#       AuthorizedKeysFile __PROGRAMDATA__/ssh/administrators_authorized_keys'
     ) | Set-Content -Path $env:ProgramData\ssh\sshd_config
 
